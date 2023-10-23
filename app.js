d3f758833c844e254b0c74d117c1e3e252421e53
jarvis09 <contact-jarvis09@laposte.net>
2023-10-23 13:18:20 +0200

    removed scrollbar for all browsers, added scroll-notice footer. refactored CSS code with ChatGPT

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var audio;
-var playPauseButton = document.getElementById('play-pause');
+var playPauseButton = document.getElementById("play-pause");
+var scrollNotice = document.getElementById("scroll-notice");
 
 
 // Setting up particles.js
-particlesJS.load('particles-js', './assets/particles.json')
+particlesJS.load("particles-js", "./assets/particles.json")
 // Setting up ityped (typing animation)
 
 ityped.init(document.querySelector("#ityped"), {
@@ -16,12 +17,12 @@ function toggleMusic() {
 
     console.log("Hello fellow dev! check out the website of the chad who fixed my code (and let me steal a good chunck of it) - QAEZZ :) (https://qaezz.dev)")
 
-    if (playPauseButton.classList.contains('fa-play')) {
-        playPauseButton.classList.remove('fa-play');
-        playPauseButton.classList.add('fa-pause');
+    if (playPauseButton.classList.contains("fa-play")) {
+        playPauseButton.classList.remove("fa-play");
+        playPauseButton.classList.add("fa-pause");
 
-        if (playPauseButton.classList.contains('first-run')) {
-            playPauseButton.classList.remove('first-run');
+        if (playPauseButton.classList.contains("first-run")) {
+            playPauseButton.classList.remove("first-run");
 
             audio = new Audio("./assets/M83-Outro.mp3");
             console.log("The cake is a lie.");
@@ -31,10 +32,18 @@ function toggleMusic() {
         audio.volume = 0.75;
 
     } else {
-        playPauseButton.classList.remove('fa-pause');
-        playPauseButton.classList.add('fa-play');
+        playPauseButton.classList.remove("fa-pause");
+        playPauseButton.classList.add("fa-play");
 
         audio.pause();
         audio.volume = 0.75;
     }
 }
+
+window.addEventListener('scroll', function() {
+    var scrollNotice = document.getElementById("scroll-notice");
+    var scrollPosition = window.scrollY; // Obtenir la position de défilement verticale
+
+    // Réglez l'opacité en fonction de la position de défilement
+    scrollNotice.style.opacity = 1 - scrollPosition / 100; // Réglage de la valeur 500 selon vos besoins
+  });
